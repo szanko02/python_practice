505580f70126ce1d4c13b776a1bbcb6a1ac32698
szanko02 <stas.060232@gmail.com>
2023-06-06 05:32:40 +0300

    imported some libs, created es and pd objects
    still sorting out how all this works

--- simple_searcher/simple_searcher.py
+++ simple_searcher/simple_searcher.py
@@ -1,5 +1,14 @@
-from flask import Flask, jsonify, request
+from flask import Flask,  request, jsonify
 from elasticsearch import Elasticsearch
+import pandas as pd
+import psycopg2
 
 app = Flask(__name__)
-es = Elasticsearch([''])
+es = Elasticsearch("http://localhost:9200")
+print(es.info().body)
+
+df = pd.read_csv("simple_searcher\posts.csv")
+print(df)
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
